f0712fd5dd0fd09a0cde36f5b2e8f184eaa58aac
Mark Vayngrib <mark@tradle.io>
2017-05-13 13:52:21 -0400

    add scripts

---
+++ ruby.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+sudo gem install cocoapods
+sudo gem install fastlane
+sudo gem install travis
+#sudo gem install fastlane
---
+++ run.sh
@@ -0,0 +1,8 @@
+
+#!/bin/sh
+
+source ./.macos
+source ./brew.sh
+source ./ruby.sh
+
+git lfs install
