c1433cf533776f9f162b8e2e3553697ad76e5937
Mark Vayngrib <mvayngrib@gmail.com>
2019-03-19 11:00:39 -0400

    add some node-related insatlls

--- node.sh
+++ node.sh
@@ -1,1 +1,7 @@
-npm i -g ytdl mvayngrib/sanitize-filename nodemon static-serves
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+nvm install node
+nvm use node
+nvm install 8.10.0 --reinstall-packages-from=node
+nvm alias default 8.10.0
+npm i -g npm@3
+npm i -g npx ytdl mvayngrib/sanitize-filename nodemon static-serves
